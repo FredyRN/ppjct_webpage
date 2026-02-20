992e59b088d13f2c1127c6ab0f7ee3b441608ac3
Fredy Rodriguez Nupia <projects@fredyrn.com>
2026-02-20 15:18:39 -0500

    created main project in Rust for backend

---
+++ blog_backend/target/debug/deps/blog_backend-218a10a294dd5895.d
@@ -0,0 +1,9 @@
+/data_store/PPJCTS/ppjct_webpage/blog_backend/target/debug/deps/blog_backend-218a10a294dd5895.d: src/main.rs Cargo.toml
+
+/data_store/PPJCTS/ppjct_webpage/blog_backend/target/debug/deps/libblog_backend-218a10a294dd5895.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
